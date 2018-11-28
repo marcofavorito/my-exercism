9dede2e03c5e6c130195cdde7a166966287b3c3c
Marco Favorito <marco.favorito@gmail.com>
2018-11-28 20:13:29 +0000

    fix bug in common-lisp:hamming

--- common-lisp/hamming/hamming.lisp
+++ common-lisp/hamming/hamming.lisp
@@ -6,15 +6,10 @@
 (in-package #:hamming)
 
 (defun distance(dna1 dna2)
-  (if (not eql (length dna1) (length dna2)) nil  
-    (let (( x 0))
+  (if (not (eql (length dna1) (length dna2))) nil  
+    (let ((x 0))
       (loop :for i :from 0 :to (- (length dna1) 1)
 	    do (if (not (eql (elt dna1 i) (elt dna2 i)))
-		 (incf x)
-		 )
-	    )
-      x
-      )
-    )
-  )
+		 (incf x)))
+      x)))
 
