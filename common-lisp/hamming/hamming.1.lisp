e23a1e28014b31d296674ba8bc44e79a87621f60
Marco Favorito <marco.favorito@gmail.com>
2018-11-30 21:43:06 +0000

    [common-lisp][hamming] restore previous solution.

---
+++ common-lisp/hamming/hamming.1.lisp
@@ -0,0 +1,14 @@
+(defpackage #:hamming
+  (:use #:cl)
+  (:export #:distance)
+  )
+
+(in-package #:hamming)
+
+(defun distance(dna1 dna2)
+  (when (= (length dna1) (length dna2))  
+    (let ((x 0))
+      (loop :for i :from 0 :to (- (length dna1) 1)
+	    do (unless (char= (elt dna1 i) (elt dna2 i))
+		 (incf x)))
+      x)))
