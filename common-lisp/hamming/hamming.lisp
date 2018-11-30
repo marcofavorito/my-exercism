56a08a20085ea6a4b01942d37795b16f39cea2ba
Marco Favorito <marco.favorito@gmail.com>
2018-11-30 21:42:04 +0000

    [common-lisp][hamming] move to more functional solution

--- common-lisp/hamming/hamming.lisp
+++ common-lisp/hamming/hamming.lisp
@@ -7,9 +7,11 @@
 
 (defun distance(dna1 dna2)
   (when (= (length dna1) (length dna2))  
-    (let ((x 0))
-      (loop :for i :from 0 :to (- (length dna1) 1)
-	    do (unless (char= (elt dna1 i) (elt dna2 i))
-		 (incf x)))
-      x)))
+    (apply #'+ 
+	 (mapcar (lambda (pair) (if (char= (first pair) (second pair)) 0 1))
+		  (mapcar #'list 
+			  (coerce dna1 'list) (coerce dna2 'list))))))
+
+
+
 
