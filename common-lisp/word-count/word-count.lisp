d9576469071bbf38f2ca03b95ce78ee92b9fd8f9
Marco Favorito <marco.favorito@gmail.com>
2018-12-07 21:47:46 +0000

    [common-lisp][word-count] completed.

--- common-lisp/word-count/word-count.lisp
+++ common-lisp/word-count/word-count.lisp
@@ -7,13 +7,15 @@
 (ql:quickload :cl-ppcre)
 
 (defun word-count (phrase)
-  (setf pairs (mapcar (lambda (x) (list (string-downcase x) 1)) (cl-ppcre::split "[ ]+" phrase)))
-
+  (setf pairs (mapcar (lambda (x) (cons (string-downcase x) 1)) (cl-ppcre::split "[ ,:!&@$%^&]+" phrase)))
+  (setf result ())
   (loop for pair in pairs do
-	(print pair))
-  
-  )
+	(progn
+	  (setf value (cdr (assoc (first pair) result :test #'equalp)))
+	  (setf value (if value value 0))
+	  (setf result (remove  (first pair) result  :test 'equal :key 'car))
+	  (push (cons (first pair) (+ value 1)) result)))
+  result)
 
 ;(reduce #'(lambda (x y) (+ (second x) (second y))) 
 
-(print (word-count "hello Hello"))
