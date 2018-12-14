67492f8003fee34f1d63f2be60ac173a10441c7b
Marco Favorito <marco.favorito@gmail.com>
2018-12-14 21:11:17 +0000

    [prolog][binary]

--- prolog/binary/binary.pl
+++ prolog/binary/binary.pl
@@ -1,11 +1,15 @@
-binary("0", 0).
-binary("1", 1).
-
 binary(Str, Dec) :-
   atom_chars(Str, Digits),
   [H|T] = Digits,
   atom_length(Digits, Length),
+  ( H\='1', H\='0' -> fail; true), 
   atom_number(H, MSB),
   string_chars(TailStr, T),
   binary(TailStr, Num),
   Dec is MSB * 2 ** (Length - 1) + Num.
+
+  
+binary("0", 0).
+binary("1", 1).
+
+
