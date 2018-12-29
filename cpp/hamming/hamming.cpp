a8114a595d40e4363de7cdb5041112ecdb5aca96
MarcoFavorito <marco.favorito@gmail.com>
2018-12-29 00:41:26 +0000

    [cpp][hamming] initial setup.

---
+++ cpp/hamming/hamming.cpp
@@ -0,0 +1,7 @@
+#include "hamming.h"
+
+int hamming::compute(std::string s1, std::string s2){
+  std::cout << s1 << s2 << std::endl; 
+  return 0;
+}
+
---
+++ cpp/hamming/hamming.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class hamming{
+  public:
+    static int compute(std::string s1, std::string s2);
+};
+
