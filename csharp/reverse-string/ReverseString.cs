51c6c95785b9606994e669abede8aa399f1e727d
MarcoFavorito <marco.favorito@gmail.com>
2019-01-10 23:20:10 +0000

    [csharp][reverse-string]

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+      char[] array = input.ToCharArray();
+      Array.Reverse(array);
+      return new string(array);
+    }
+}
