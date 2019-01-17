c5e1157bff7c81723a688ef444566680b1dd255f
MarcoFavorito <marco.favorito@gmail.com>
2019-01-17 19:22:34 +0000

    [charp][grains]

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n <= 0 || n > 64) throw new ArgumentOutOfRangeException(); 
+        return (ulong) Math.Pow(2, n - 1);
+    }
+
+    public static ulong Total()
+    {
+        return UInt64.MaxValue; 
+    }
+}
