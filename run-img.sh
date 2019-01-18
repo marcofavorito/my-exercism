193e7afd1613d5022654799749bc72b7f5d36758
MarcoFavorito <marco.favorito@gmail.com>
2019-01-18 19:18:28 +0000

    fix run script.

--- run-img.sh
+++ run-img.sh
@@ -4,14 +4,14 @@ DOCKER_IMAGES_HOME="./.dotfiles/docker"
 RUN_SCRIPT="scripts/docker-run.sh"
 
 if [ $# == 0 ]; then
-  "${DOCKER_IMAGES_HOME}"/my-exercism/"${RUN_SCRIPT}"
+  eval "${DOCKER_IMAGES_HOME}"/my-exercism/"${RUN_SCRIPT}"
   exit 0
 fi
 
 TRACK=$(ls -1 -d */ | sed "s/\(.*\)\//\1/g" | grep -E "^$1$")
 
 if [ "$TRACK" != "" ]; then
-  "${DOCKER_IMAGES_HOME}"/"${TRACK}"/"${RUN_SCRIPT}"
+  eval "${DOCKER_IMAGES_HOME}/${TRACK}/${RUN_SCRIPT} -- /bin/bash"
   exit 0
 else
   echo "Track not available."
