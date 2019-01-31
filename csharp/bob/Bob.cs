18c5be702721c3b838d81a3716b60262d15ec21a
MarcoFavorito <marco.favorito@gmail.com>
2019-01-31 19:43:17 +0000

    [csharp][bob] fix

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -14,26 +14,26 @@ public static class Bob
         new Regex(@"[a-z]|^[^A-Za-z]*$|[a-z][A-Z]|[A-Z][a-z]", RegexOptions.Compiled);
     public static Regex nothingRegex = new Regex(@"^[^A-Za-z0-9]*$", RegexOptions.Compiled);
     
-    public static bool isYelled(string statement){
-        return isNotYelledRegex.Matches(statement).Count == 0;
-    }
+    public static bool isYelled(string statement) =>
+        isNotYelledRegex.Matches(statement).Count == 0;
 
-    public static bool isNothing(string statement){
-        return nothingRegex.Matches(statement).Count != 0;
-    }
+    public static bool isNothing(string statement) => 
+        nothingRegex.Matches(statement).Count != 0;
+
+    public static bool isQuestion(string statement) =>
+        statement.Length > 0 && statement[statement.Length - 1] == '?';
 
     public static string Response(string statement)
     {
         string s = statement.TrimStart().TrimEnd();
-        if (s.Length > 0 && s[s.Length - 1] == '?'){
-            if(isYelled(s)) return YELL_QUESTION;
-            else return QUESTION;
-        }
-        else if (isNothing(s)) return ADDRESS;
-        else{
-            if(isYelled(s)) return YELL;
-            else return ANYTHING;
-        }
+        bool yelled = isYelled(s);
+        bool nothing = isNothing(s);
+        bool question = isQuestion(s);
+        if (question && yelled) return YELL_QUESTION;
+        else if (question && !yelled) return QUESTION;
+        else if (nothing) return ADDRESS;
+        else if (yelled) return YELL;
+        else return ANYTHING;
 
     }
 }
